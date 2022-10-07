9eb66aae140fc94c7270c1e56ae9b415460974be
Carl Noval <cjrnoval22@gmail.com>
2022-10-08 03:22:21 +0900

    added locators as class variable tuples

--- pages/result.py
+++ pages/result.py
@@ -3,8 +3,13 @@ This module contains DuckDuckGoResultPage,
 the page object for the DuckDuckGo search result page.
 """
 
+# manually entered from course
+from selenium.webdriver.common.by import By
 
 class DuckDuckGoResultPage:
+
+  RESULT_LINKS = (By.XPATH, "//a[@data-testid='result-title-a']")
+  SEARCH_INPUT = (By.ID, "search_form_input")
 
   # dunder init method constructor
   def __init__(self, browser):
--- pages/search.py
+++ pages/search.py
@@ -3,8 +3,14 @@ This module contains DuckDuckGoSearchPage,
 the page object for the DuckDuckGo search page.
 """
 
+# manually entered from course
+from selenium.webdriver.common.by import By
+
 
 class DuckDuckGoSearchPage:
+
+  # locators as tuple class variables since only one immutable varaibale is needed
+  SEARCH_INPUT = (By.ID, "search_form_input_homepage")
 
   # dunder init method constructor
   def __init__(self, browser):
@@ -17,4 +23,3 @@ class DuckDuckGoSearchPage:
   def search(self, phrase):
     # TODO
     pass
-  
