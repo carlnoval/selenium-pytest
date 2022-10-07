1b3ff69c59112670658935b4aaaffe11009d7ae5
Carl Noval <cjrnoval22@gmail.com>
2022-10-08 01:49:40 +0900

    setup of page objects and using page objects in test case

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,21 +2,33 @@
 These tests cover DuckDuckGo searches.
 """
 
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+
+
 def test_basic_duckduckgo_search(browser):
+  # webdriverr used to construct page object instances
+  search_page = DuckDuckGoSearchPage(browser)
+  result_page = DuckDuckGoResultPage(browser)
+  PHRASE = "panda"
+  
+  # Given the DuckDuckGo home page is displayed
+  search_page.load()
 
-    # Given the DuckDuckGo home page is displayed
-    # TODO
+  # When the user searches for "panda"
+  search_page.search(PHRASE)
 
-    # When the user searches for "panda"
-    # TODO
+  # Then the search result title contains "panda"
+  assert PHRASE in result_page.title()
+  
+  # And the search result query is "panda"
+  assert PHRASE == result_page.search_input_value()
+  
+  # And the search result links pertain to "panda"
+  titles = result_page.result_link_titles()
+  matches = [t for t in titles if PHRASE.lower() in t.lower()]
+  assert len(matches) > 0
 
-    # Then the search result title contains "panda"
-    # TODO
-    
-    # And the search result query is "panda"
-    # TODO
-    
-    # And the search result links pertain to "panda"
-    # TODO
-
-    raise Exception("Incomplete Test")
+  # TODO: Remove this exception once the test is complete
+  # Test is not complete yet since page objects are still not done
+  raise Exception("Incomplete Test")
